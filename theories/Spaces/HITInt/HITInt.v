f668afe6bd8130291c008cf4d49ad0c96c7cf6a5
heimerzieher <heimerzieher@me.com>
2024-10-03 23:57:42 -0400

    Added function to ordinary integers, proved its right invertible

--- theories/Spaces/HITInt/HITInt.v
+++ theories/Spaces/HITInt/HITInt.v
@@ -25,7 +25,7 @@ Module Export IntegersHIT.
   (* Check IntegersHIT_ind. *)
 
   (* We define the induction principle. We need to use Fixpoint because its recursive*)
-  Fixpoint IntegersHIT_ind'
+  Fixpoint IntegersHIT_ind
     (P : IntegersHIT -> Type@{k})
     (t0 : P zero_i)
     (f : forall z : IntegersHIT, P z -> P (succ z))
@@ -37,15 +37,15 @@ Module Export IntegersHIT.
     {struct x} : P x  
     := match x  with
     | zero_i => fun _ _ => t0
-    | succ z => fun _ _ =>  f z (IntegersHIT_ind' P t0 f g1 g2 s r z)
-    | pred1 z => fun _ _ =>  g1 z (IntegersHIT_ind' P t0 f g1 g2 s r z)
-    | pred2 z => fun _ _ =>  g2 z (IntegersHIT_ind' P t0 f g1 g2 s r z)
+    | succ z => fun _ _ =>  f z (IntegersHIT_ind P t0 f g1 g2 s r z)
+    | pred1 z => fun _ _ =>  g1 z (IntegersHIT_ind P t0 f g1 g2 s r z)
+    | pred2 z => fun _ _ =>  g2 z (IntegersHIT_ind P t0 f g1 g2 s r z)
     end s r.
-    (*We need somehow make this depend on s and r as well*)
+    (*This has to be dependent on s and r as well*)
 
 
-  Check IntegersHIT_ind'.
-  About IntegersHIT_ind'.
+  Check IntegersHIT_ind.
+  About IntegersHIT_ind.
 
   Axiom IntegersHIT_ind_beta_sec_sec
   : forall (P : IntegersHIT -> Type@{k})
@@ -56,7 +56,7 @@ Module Export IntegersHIT.
     (s : forall (z : IntegersHIT) (t : P(z)), (sec z # (g1 (succ z) (f z t)) = t))
     (r : forall (z : IntegersHIT) (t : P(z)), (ret z # (f (pred2 z) (g2 z t)) = t))
     (z: IntegersHIT),
-    (let f':= IntegersHIT_ind' P t0 f g1 g2 s r  in
+    (let f':= IntegersHIT_ind P t0 f g1 g2 s r  in
     ((apD f' (sec z)) = s z (f' z))).
 
 Print IntegersHIT_ind_beta_sec_sec.
@@ -70,14 +70,14 @@ Print IntegersHIT_ind_beta_sec_sec.
     (s : forall (z : IntegersHIT) (t : P(z)), (sec z # (g1 (succ z) (f z t)) = t))
     (r : forall (z : IntegersHIT) (t : P(z)), (ret z # (f (pred2 z) (g2 z t)) = t))
     (z: IntegersHIT),
-    (let f':= IntegersHIT_ind' P t0 f g1 g2 s r  in
+    (let f':= IntegersHIT_ind P t0 f g1 g2 s r  in
     ((apD f' (ret z)) = r z (f' z))).
 
  End IntegersHIT.
 End IntegersHIT.
 
 Check transport_const.
-Check IntegersHIT_ind'.
+Check IntegersHIT_ind.
 
 Definition IntegersHIT_rec
   (P: Type@{k})
@@ -89,7 +89,7 @@ Definition IntegersHIT_rec
   (r : forall  (t : P ), (f (g2 t)= t))
   : IntegersHIT -> P.
 Proof.
-  srapply IntegersHIT_ind'.
+  srapply IntegersHIT_ind.
   1: exact t0.
   1: exact (fun _ => f).
   1: exact (fun _ => g1).
@@ -109,42 +109,79 @@ Proof.
   1: exact int_succ_pred.
   1: exact int_pred_succ.
 Defined.
-(* 
-Fixpoint IntITtoIntHIT 
+
+Definition IntITtoIntHIT
   (z : Int)
-  : IntegersHIT
-  := match z  with
-  | zero => zero_i
-  | negS 0 => (pred1 zero_i)
-  | negS (S n) => (pred1 (IntITtoIntHIT (negS n)))
-  | posS 0 => (succ zero_i)
-  | posS (S n) => (succ (IntITtoIntHIT (posS n)))
-  end.
-
-
-  (* apply IntegersHIT_rec Int 0 
-   *)
-
-
-  
-
-  (*1: exact (fun _ => f). *)
-(* Abort. *)
-
-Definition IntegersHIT_rec
-
-
-(* 
-Definition IntegersHIT_rec {P} (c : A -> P) (g : forall a b, R a b -> c a = c b)
-  : GraphQuotient R -> P.
+  : IntegersHIT.
 Proof.
-  srapply GraphQuotient_ind.
-  1: exact c.
-  intros a b s.
-  refine (transport_const _ _ @ g a b s).
-Defined. *)
+  induction z.
+  -
+    exact zero_i.
+  - 
+    exact (succ IHz).
+  -
+    exact (pred1 IHz).
+Defined.
 
- *)
+Compute IntITtoIntHIT (posS 5).
+
+Compute IntITtoIntHIT (negS 1).
+
+Compute IntITtoIntHIT zero.
+
+Compute IntHITtoIntIT zero_i.
+
+Compute IntHITtoIntIT (succ (pred2 zero_i)).
+
+Compute IntHITtoIntIT (pred1 (succ (pred2 zero_i))).
+
+Compute IntHITtoIntIT (succ (succ (succ zero_i))).
+
+Definition int_succ_is_same 
+(x y : Int) 
+(t: (x = y))
+: (int_succ x) = (int_succ y).
+Proof.
+  simpl.
+  induction t.
+  - reflexivity.
+Defined. 
+
+Definition int_pred_is_same 
+(x y : Int) 
+(t: (x = y))
+: (int_pred x) = (int_pred y).
+Proof.
+  simpl.
+  induction t.
+  - reflexivity.
+Defined. 
+
+Definition linv
+ (z : Int )
+ : ((IntHITtoIntIT o IntITtoIntHIT) z) = z.
+Proof.
+  induction z as [|[|z] IHz|[|z] IHz].
+  -
+    simpl.
+    reflexivity.
+  -
+    simpl.
+    reflexivity.
+  - 
+    simpl.
+    apply int_succ_is_same in IHz.
+    apply IHz. 
+  -
+    simpl.
+    reflexivity.
+  -
+    simpl.
+    apply int_pred_is_same in IHz.
+    apply IHz.
+Defined.
+
+
 
 
 
