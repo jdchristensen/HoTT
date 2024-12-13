7b2cf8ca9a70166cdfda3feb52acf9c116baf4ad
Ben Connors <benconnors@outlook.com>
2024-12-12 19:06:07 -0500

    PushoutPath: Add zigzag construction

--- theories/PushoutPath/PushoutPath.v
+++ theories/PushoutPath/PushoutPath.v
@@ -47,3 +47,153 @@ Section InductiveStep.
     (iota_step a) == (dot_step b a r) o (dot a b r) 
     := fun a b r p => (pglue ((b ; r) , p)).
 End InductiveStep.
+
+Section ZigzagIdentity.
+
+  (** Construct the identity zigzag sequence. *)
+
+  Context {A B : Type} (R : A -> B -> Type) (a0 : A).
+
+  (** Use a record type for a full step to avoid the interleaved sequence and [flip R]. *)
+  Record Zig : Type := {
+    P : Family A;
+    Q : Family B;
+    concatQP : Dot (flip R) Q P;
+  }.
+
+  Definition Qsucc (Z : Zig) : Family B
+    := family_step (flip R) (concatQP Z).
+
+  Definition concatPQ (Z : Zig) : Dot R (P Z) (Qsucc Z)
+    := dot_step (flip R) (concatQP Z).
+
+  Definition Psucc (Z : Zig) : Family A
+    := family_step R (concatPQ Z).
+
+  Definition concatQPsucc (Z : Zig) : Dot (flip R) (Qsucc Z) (Psucc Z)
+    := dot_step R (concatPQ Z).
+
+  Definition zigzag_step (Z : Zig) : Zig
+    := Build_Zig (Psucc Z) (Qsucc Z) (concatQPsucc Z).
+
+  (** The initial zigzag: over [A] we have the identity type at [a0] and over [B] we have the empty type; these should be thought of as paths of length 0 and -1, respectively. *)
+  Definition zigzag_initial : Zig.
+  Proof.
+    snrapply Build_Zig.
+    - exact (fun a => a0 = a). (** Define [P0 := Id a0] *)
+    - exact (fun b => Empty). (** Define [Q0 := Empty] *)
+  - intros b a r q; destruct q. (** Define [Q0 -> P_0] *)
+  Defined.
+
+  Definition zigzag : nat -> Zig
+    := fun n => nat_iter n zigzag_step zigzag_initial.
+
+  Definition zigzag_P : A -> Sequence.
+  Proof.
+    intro a.
+    snrapply Build_Sequence.
+    - intro n; exact (P (zigzag n) a).
+    - intro n. cbn. apply iota_step.
+  Defined.
+
+  Definition zigzag_Q : B -> Sequence.
+  Proof.
+    intro b.
+    snrapply Build_Sequence.
+    - intro n; exact (Q (zigzag n) b).
+    - intro n. cbn. apply iota_step.
+  Defined.
+
+  Definition zigzag_gluePQ {a : A} {b : B} (r : R a b) 
+    : DiagramMap (zigzag_P a) (succ_seq (zigzag_Q b)).
+  Proof.
+    snrapply Build_DiagramMap.
+    - intro n; exact (concatPQ (zigzag n) a b r).
+    - intros n m g x.
+      destruct g.
+      lhs nrapply homotopy_step.
+      apply ap.
+      symmetry.
+      apply homotopy_step.
+  Defined.
+
+  Definition zigzag_glueQP {a : A} {b : B} (r : R a b) 
+: DiagramMap (zigzag_Q b) (zigzag_P a).
+  Proof.
+    snrapply Build_DiagramMap.
+    - intro n; exact (concatQP (zigzag n) b a r).
+    - intros n m g x.
+      destruct g.
+      lhs nrapply homotopy_step.
+      apply ap.
+      symmetry.
+      apply homotopy_step.
+  Defined.
+
+  Definition zigzag_gluePQP {a : A} {b : B} (r : R a b) (n : nat)
+    : (fun (x : zigzag_P a n) => x^+) == zigzag_glueQP r (S n) o zigzag_gluePQ r n
+    := (homotopy_step R _ a b r).
+
+  Definition zigzag_glueQPQ {a : A} {b : B} (r : R a b) (n : nat)
+    : (fun (x : zigzag_Q b n) => x^+) == zigzag_gluePQ r n o zigzag_glueQP r n
+    := (homotopy_step (flip R) _ b a r).
+
+  (** The colimit of paths starting and ending in A. *)
+  Definition zigzag_Pinf (a : A) : Type
+    := Colimit (zigzag_P a).
+
+  (** Our candidate for reflexivity: the colimit of reflexivity. *)
+  Definition zigzag_refl : zigzag_Pinf a0
+    := @colim _ (zigzag_P a0) 0%nat idpath.
+
+  (** The colimit of paths starting in A and ending in B. *)
+  Definition zigzag_Qinf (b : B) : Type 
+    := Colimit (zigzag_Q b).
+
+  Section GluingEquiv.
+
+    Context {a : A} {b : B} (r : R a b).
+
+    (** The gluing equivalence. *)
+    Definition equiv_zigzag_glueinf
+      : (zigzag_Pinf a) <~> (zigzag_Qinf b)
+      := equiv_zigzag_glue (zigzag_glueQP r) (zigzag_gluePQ r) (zigzag_glueQPQ r) (zigzag_gluePQP r).
+
+    Definition zigzag_gluePQinf : zigzag_Pinf a -> zigzag_Qinf b
+      := equiv_zigzag_glueinf.
+
+    Definition zigzag_glueQPinf : zigzag_Qinf b -> zigzag_Pinf a
+      := equiv_zigzag_glueinf^-1.
+
+    Definition zigzag_comp_eissect (n : nat) (p : zigzag_P a n) : (eissect equiv_zigzag_glueinf (@colim _ (zigzag_P a) n p)) = (ap (@colim _ (zigzag_P a) (S n)) (zigzag_gluePQP r n p)^) @ (@colimp _ (zigzag_P a) n _ _ p)
+      := zigzag_comp_eissect (zigzag_glueQP r) (zigzag_gluePQ r) (zigzag_glueQPQ r) (zigzag_gluePQP r) n p.
+
+    Definition zigzag_comp_eisretr (n : nat) (q : zigzag_Q b n) : (eisretr equiv_zigzag_glueinf (@colim _ (zigzag_Q b) n q)) = (ap (@colim _ (zigzag_Q b) (S n)) (zigzag_glueQPQ r n q)^) @ (@colimp _ (zigzag_Q b) n _ _ q)
+      := zigzag_comp_eisretr (zigzag_glueQP r) (zigzag_gluePQ r) (zigzag_glueQPQ r) (zigzag_gluePQP r) n q.
+
+  End GluingEquiv.
+
+  (** Prove that the colimit of the identity zigzag is equivalent to the identity type for pushouts. *)
+
+  (** FIXME: This is essentially [SpanPushout]. *)
+
+  Definition relation_total : Type
+    := {x : A * B | R (fst x) (snd x)}.
+
+  Definition relation_pushout : Type.
+  Proof.
+    snrapply Pushout.
+    + exact relation_total.
+    + exact A.
+    + exact B.
+    + exact (fst o pr1).
+    + exact (snd o pr1).
+  Defined.
+
+  Context `{Univalence}.
+
+  (** The candidate for the identity type. *)
+  Definition zigzag_family_half
+    : relation_pushout -> Type
+    := fam_podescent (Build_poDescent _ _ _ _ _ zigzag_Pinf zigzag_Qinf (fun x => equiv_zigzag_glueinf (pr2 x))).
+End ZigzagIdentity.
