b43ca8cbfceed8351c595f5e97080e11feb35764
Dan Christensen <jdc@uwo.ca>
2025-02-07 15:21:40 -0500

    BarInduction: lots of minor cleanups

--- theories/BarInduction.v
+++ theories/BarInduction.v
@@ -5,35 +5,36 @@ Require Import Spaces.Nat.Core.
 Require Import Spaces.Finite.FinNat.
 Require Import Misc.UStructures.
 Require Import Spaces.NatSeq.Core Spaces.NatSeq.UStructure.
-Require Import Spaces.List.Core Spaces.List.Paths Spaces.List.Theory.
-Require Import Algebra.Rings.Vector.
+Require Import Spaces.List.Core Spaces.List.Theory.
 Require Import BoundedSearch.
 
 Local Open Scope nat_scope.
 Local Open Scope type_scope.
 Local Open Scope list_scope.
 
-(** A family [B] on a type of lists is a bar if every infinite sequence restricts to a finite sequence in [B]. *)
+(** ** The basic definitions *)
+
+(** A family [B] on a type of lists is a bar if every infinite sequence restricts to a finite sequence satisfying [B]. *)
 Definition is_bar {A : Type} (B : list A -> Type)
   := forall (s : nat -> A), {n : nat & B (list_restrict s n)}.
 
-(** A bar is uniform if there is an upper bound for the length of restrictions of infinite sequences in it. *)
+(** A family [B] is a uniform bar if it is a bar such that there is an upper bound for the lengths of the restrictions needed to satisfy the bar condition. *)
 Definition is_uniform_bar {A : Type} (B : list A -> Type)
   := {M : nat & forall (s : nat -> A),
-                  {n : (FinNat M) & B (list_restrict s n.1)}}.
+                  {n : FinNat M & B (list_restrict s n.1)}}.
 
-(** A family [B] on a type of finite sequences is inductive if, for every list, concatenations with any term being in [B] implies that the list is in [B]. *)
+(** A family [B] on a type of finite sequences is inductive if, for every list [l], if the concatenation of [l] with any term satisfies [B], then the list satisfies [B]. *)
 Definition is_inductive {A : Type} (B : list A -> Type)
   := forall (l : list A), (forall (a : A), B (l ++ [a])) -> B l.
 
-(** A family [B] on a type of finite sequences is monotone if for every list in [B] concatenation with any other list is still in [B]. Equivalently, we can just check the concatenations with lists of length one. *)
+(** A family [B] on a type of finite sequences is monotone if for every list satisfying [B], the concatenation with any other list still satisfies [B]. Equivalently, we can just check the concatenations with lists of length one. *)
 Definition is_monotone {A : Type} (B : list A -> Type)
   := forall (l1 l2 : list A), B l1 -> B (l1 ++ l2).
 
-Definition is_monotone' {A : Type} (B : list A ->Type)
+Definition is_monotone' {A : Type} (B : list A -> Type)
   := forall (l : list A) (a : A), B l -> B (l ++ [a]).
 
-Definition is_monotone'_is_monotone {A : Type} (B : list A -> Type)
+Definition is_monotone_is_monotone' {A : Type} (B : list A -> Type)
   (mon : is_monotone' B)
   : is_monotone B.
 Proof.
@@ -44,7 +45,7 @@ Proof.
     apply IHl2, mon, h.
 Defined.
 
-(** We state three forms of bar induction. Monotone bar induction implies decidable bar induction and full bar induction implies both others. *)
+(** We state three forms of bar induction: (full) bar induction, monotone bar induction and decidable bar induction. *)
 
 Definition decidable_bar_induction (A : Type) :=
   forall (B : list A -> Type)
@@ -66,9 +67,8 @@ Definition bar_induction (A : Type) :=
   (bar : is_bar B),
   B nil.
 
-(** Monotone and full bar induction can be more generally stated for two families. The definitions are equivalent. *)
+(** The three bar induction principles can be more generally stated for two families. It's clear that the two-family versions imply the one-family versions. We show the reverse implications for full bar induction and monotone bar induction. Are the definitions also equivalent in the decidable case? *)
 
-(** Are the definitions also equivalent in the decidable case? *)
 Definition decidable_bar_induction' (A : Type) :=
   forall (B C : list A -> Type)
   (sub : forall l : list A, C l -> B l)
@@ -96,7 +96,7 @@ Definition bar_induction'_bar_induction (A : Type) (BI : bar_induction A)
   : bar_induction' A
   := fun B C sub indB barC => BI B indB (fun s => ((barC s).1; sub _ (barC s).2)).
 
-Definition monotone_bar_induction'_montone_bar_induction (A : Type)
+Definition monotone_bar_induction'_monotone_bar_induction (A : Type)
   (MBI : monotone_bar_induction A)
   : monotone_bar_induction' A.
 Proof.
@@ -119,49 +119,52 @@ Proof.
     apply sub, monC, barC.
 Defined.
 
+(** ** Relations between the three forms of bar induction *)
+
+(** Full bar induction clearly implies the other two. We now show that monotone bar induction implies decidable bar induction, by passing through the two-family versions. *)
+
 Definition decidable_bar_induction'_monotone_bar_induction' (A : Type)
   (MBI : monotone_bar_induction' A)
   : decidable_bar_induction' A.
 Proof.
   intros B C sub dC indB barC.
-  pose (P := fun l => {n : nat & (n <= length l) * C (take n l)}).
-  pose (Q := fun l => B l + P l).
-  assert (dP : forall l : list A, Decidable (P l)).
-  1: intro l; rapply decidable_search.
-  assert (hqb : Q nil -> B nil).
-  { intro q; destruct q as [b0|[n [hn hc]]].
-    1: exact b0.
-    exact (sub _ (take_nil _ # hc)). }
-  apply hqb.
+  (* The [n <= length l] part is redundant, but makes it clear that [P l] is decidable, which is used below. *)
+  pose (P l := {n : nat & (n <= length l) * C (take n l)}).
+  pose (Q l := B l + P l).
+  (* First we show that it is enough to prove [Q nil], and then we prove [Q nil]. *)
+  enough (Q nil) as [b0 | [n [hn hc]]].
+  1: exact b0.
+  1: exact (sub _ (take_nil _ # hc)).
   apply (MBI Q P (fun l pl => inr pl)).
   - intros l1 l2 [n (cn1, cn2)].
-    exists n; constructor.
-    + rewrite length_app.
-      apply (_ cn1).
+    exists n; split.
+    + rewrite length_app; exact _.
     + exact (take_app l1 l2 cn1 # cn2).
   - intros l hl.
-    destruct (dP l) as [t|f].
+    assert (dP : Decidable (P l)) by rapply decidable_search.
+    destruct dP as [t|f].
     1: exact (inr t).
-    left. refine (indB _ _).
-    intro a; destruct (hl a) as [b|[n [hn hc]]].
+    left. apply indB.
+    intro a; destruct (hl a) as [b | [n [hn hc]]].
     1: exact b.
-    destruct (equiv_leq_lt_or_eq hn) as [hn1|hn2].
+    destruct (equiv_leq_lt_or_eq hn) as [hn1 | hn2]; clear hn.
     + contradiction f.
       assert (hln : n <= length l).
-      { rewrite length_app, nat_add_comm in hn1.
-        exact _. }
+      { rewrite length_app, nat_add_comm in hn1. cbn in hn1; unfold "<" in hn1.
+        exact (leq_pred' hn1). }
       exists n; constructor.
       * exact hln.
       * exact ((take_app l [a] hln)^ # hc).
     + refine (sub _ ((take_length_leq _ _ _) # hc)).
-      apply equiv_leq_lt_or_eq; exact (inr hn2^).
+      by destruct hn2^.
   - intro s.
-    exists (barC s).1; exists (barC s).1.
-    constructor.
-    + by rewrite (length_list_restrict s (barC s).1).
-    + rewrite (take_length_leq _ _).
+    pose (n := (barC s).1).
+    exists n; exists n.
+    split.
+    + by rewrite length_list_restrict.
+    + rewrite take_length_leq.
       1: exact (barC s).2.
-      by rewrite (length_list_restrict s (barC s).1).
+      by rewrite length_list_restrict.
 Defined.
 
 Definition decidable_bar_induction_monotone_bar_induction (A : Type)
@@ -169,16 +172,17 @@ Definition decidable_bar_induction_monotone_bar_induction (A : Type)
   : decidable_bar_induction A
   := fun B dec ind bar =>
       (decidable_bar_induction'_monotone_bar_induction' A
-        (monotone_bar_induction'_montone_bar_induction A MBI))
+        (monotone_bar_induction'_monotone_bar_induction A MBI))
       B B (fun _ => idmap) dec ind bar.
 
-(** * Full bar induction for a type implies that it is compact.  *)
+(** ** Implications of bar induction *)
 
-Definition BI_sig_family {A : Type} (P : A -> Type) (l : list A)
-  : Type
+(** Full bar induction for a type [A] implies that it is Sigma-compact, in the sense that any decidable family over [A] has a decidable Sigma-type. To prove this, we begin by defining a type family [P] on [list A] so that [P nil] i s our goal. *)
+
+Definition BI_sig_family {A : Type} (P : A -> Type) (l : list A) : Type
   := match l with
       | nil => Decidable {a : A & P a}
-      | n :: l' => ~(P n)
+      | a :: l' => ~(P a)
      end.
 
 Definition is_bar_BI_sig_family {A : Type} {P : A -> Type}
@@ -197,9 +201,8 @@ Defined.
 Definition is_inductive_BI_sig_family {A : Type} (P : A -> Type)
   : is_inductive (BI_sig_family P).
 Proof.
-  intros l h.
-  destruct l.
-  - right. exact (fun pa => (h pa.1) pa.2).
+  intros [|a l] h.
+  - right. exact (fun pa => h pa.1 pa.2).
   - exact (h a).
 Defined.
 
@@ -219,7 +222,7 @@ Proof.
   - left; exact (fun n => stable (fun z : s n <> 0 => r (n; z))).
 Defined.
 
-(** * The Fan theorem.  *)
+(** ** The fan theorem *)
 
 Definition decidable_fan_theorem (A : Type) :=
   forall (B : list A -> Type)
@@ -238,7 +241,7 @@ Definition fan_theorem (A : Type) :=
   (bar : is_bar B),
   is_uniform_bar B.
 
-(** The family we use to apply the fan theorem in our proof that continuity implies uniform continuity *)
+(** The family we use when applying the fan theorem in our proof that continuity implies uniform continuity. *)
 Definition uc_theorem_family {A : Type} (p : (nat -> A) -> Bool)
   : list A -> Type
   := fun l =>
@@ -250,30 +253,29 @@ Definition is_bar_uc_theorem_family {A : Type}
   : is_bar (uc_theorem_family p).
 Proof.
   intro s.
-  specialize (conn s 0); unfold trivial_us, us_rel in conn.
-  exists conn.1.
-  unfold uc_theorem_family.
+  specialize (conn s 0) as [n H].
+  exists n.
   intros u v h t.
   rewrite length_list_restrict in h, t.
-  assert (y : s =[conn.1] u).
-  { symmetry; apply list_restrict_eq_iff_seq_agree_lt, h. }
-  refine ((conn.2 _ y)^ @ conn.2 _ _).
-  transitivity u.
-  - exact y.
-  - exact t.
+  symmetry in h.
+  apply list_restrict_eq_iff_seq_agree_lt in h.
+  refine ((H _ h)^ @ H _ _).
+  exact (transitivity h t).
 Defined.
 
-(** The fan theorem implies that every continuous function is uniformly continuous. Current proof uses the full fan theorem. Less powerful versions might be enough. *)
+(** The fan theorem implies that every continuous function is uniformly continuous. The current proof uses the full fan theorem. Less powerful versions might be enough. *)
 
 Definition uniform_continuity_fan_theorem {A : Type} (fan : fan_theorem A)
   (p : (nat -> A) -> Bool) (con : IsContinuous p)
   : uniformly_continuous p.
 Proof.
-  pose (fanapp := fan (uc_theorem_family p) (is_bar_uc_theorem_family p con)).
-  exists fanapp.1.
+  pose proof (fanapp := fan (uc_theorem_family p) (is_bar_uc_theorem_family p con)).
+  destruct fanapp as [n ub].
+  intro m.
+  exists n.
   intros u v h.
-  apply (fanapp.2 u).2.
+  apply (ub u).2.
   - exact (ap _ (length_list_restrict _ _)).
   - rewrite length_list_restrict.
-    exact ((us_rel_leq (_ (fanapp.2 u).1.2) h)).
+    exact ((us_rel_leq (_ (ub u).1.2) h)).
 Defined.
