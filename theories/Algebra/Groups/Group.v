a838ee27720ee6a3fdbddf3532581e31a030503b
Dan Christensen <jdc@uwo.ca>
2026-07-28 16:23:24 -0400

    Group: more cleanups

--- theories/Algebra/Groups/Group.v
+++ theories/Algebra/Groups/Group.v
@@ -657,7 +657,7 @@ Definition grp_pow_succ' {G : Group} (n : Int) (g : G)
 Proof.
   simpl.
   revert n.
-  rapply (int_homotopic_two_fun_equiv (fun h => g * h) _ _); cbn beta.
+  rapply (int_homotopic_two_fun_equiv (g *.) _ _); cbn beta.
   - unfold grp_pow.
     lhs napply grp_unit_r.
     by rhs napply grp_unit_l.
@@ -713,7 +713,7 @@ Definition grp_pow_int_mul {G : Group} (m n : Int) (g : G)
   : grp_pow g (m * n)%int = grp_pow (grp_pow g m) n.
 Proof.
   revert n.
-  rapply (int_homotopic_two_fun_equiv (fun h => grp_pow g m * h) _ _); cbn beta; cbn beta.
+  rapply (int_homotopic_two_fun_equiv (grp_pow g m *.) _ _); cbn beta.
   - simpl.
     by rewrite int_mul_0_r.
   - intro n.
@@ -728,7 +728,7 @@ Definition grp_pow_commutes {G : Group} (n : Int) (g h : G)
   : h * (grp_pow g n) = (grp_pow g n) * h.
 Proof.
   revert n.
-  rapply (int_homotopic_two_fun_equiv (fun h => h * g) _ _); cbn beta; cbn beta.
+  rapply (int_homotopic_two_fun_equiv (.* g) _ _); cbn beta.
   - by apply grp_g1_1g.
   - intro n.
     rewrite <- simple_associativity.
@@ -753,7 +753,7 @@ Definition grp_pow_mul {G : Group} (n : Int) (g h : G)
   : grp_pow (g * h) n = (grp_pow g n) * (grp_pow h n).
 Proof.
   revert n.
-  rapply (int_homotopic_two_fun_equiv (fun k => (g * h) * k) _ _); cbn beta.
+  rapply (int_homotopic_two_fun_equiv ((g * h) *.) _ _); cbn beta.
   - simpl.
     symmetry; napply grp_unit_r.
   - reflexivity.
