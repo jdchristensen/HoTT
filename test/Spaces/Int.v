e4378b7e401ecc09c67d9a9a131865f7450e5a7a
heimerzieher <heimerzieher@me.com>
2026-07-30 11:14:20 +0200

    test/Spaces/Int.v: Added further tests

--- test/Spaces/Int.v
+++ test/Spaces/Int.v
@@ -9,17 +9,23 @@ Definition test2 : int_to_sint (int_succ_sect zero) = sint_NegS 0 := idpath.
 
 Definition test3 : int_to_sint (int_succ (int_succ_sect zero)) = sint_zero := idpath.
 
-Definition test4 : sint_to_int sint_zero = zero := idpath.
+Definition test4 : int_to_sint (int_neg (int_succ (int_succ zero))) = sint_NegS 1 := idpath.
 
-Definition test5 : sint_to_int (sint_PosS 0) = int_succ zero := idpath.
+Definition test5 : int_to_sint (-4)%int = sint_NegS 3 := idpath.
 
-Definition test6 : sint_to_int (sint_NegS 0) = int_pred zero := idpath.
+Definition test6 : int_to_sint ((0.+1)%int) = sint_PosS 0 := idpath.
+
+Definition test7 : int_to_sint ((0.+1)-1)%int = sint_zero := idpath.
+
+Definition test8 : sint_to_int sint_zero = zero := idpath.
+
+Definition test9 : sint_to_int (sint_PosS 0) = int_succ zero := idpath.
+
+Definition test10 : sint_to_int (sint_NegS 0) = int_pred zero := idpath.
 
 (** Test the reduction functions for int. *)
-Definition test7 : int_reduce (int_succ (int_pred (int_succ zero))) = int_succ zero := idpath.
+Definition test11 : int_reduce (int_succ (int_pred (int_succ zero))) = int_succ zero := idpath.
 
-Definition test8: int_reduce (int_succ_sect zero) = int_pred zero := idpath. 
+Definition test12: int_reduce (int_succ_sect zero) = int_pred zero := idpath. 
 
-Definition test9 : int_reduce (int_pred (int_succ (int_succ_sect zero))) = int_pred zero := idpath.
-
-
+Definition test13 : int_reduce (int_pred (int_succ (int_succ_sect zero))) = int_pred zero := idpath.
